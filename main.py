56eeed35609cdb3b0dbd63f4d6be0feb76298a6a
al064697 <al064697@uacam.mx>
2023-10-16 23:00:38 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import string
+import random
+
+def generar_contraseña(longitud):
+    if longitud < 4:
+        print("La longitud debe ser al menos 4")
+        return
+
+    # Generar cada tipo de caracter y agregarlo a la contraseña
+    contraseña = []
+    contraseña.append(random.choice(string.ascii_uppercase))
+    contraseña.append(random.choice(string.ascii_lowercase))
+    contraseña.append(random.choice(string.digits))
+    contraseña.append(random.choice(string.punctuation))
+
+    # Rellenar el resto de la contraseña con caracteres aleatorios
+    for i in range(longitud - 4):
+        contraseña.append(random.choice(string.ascii_letters + string.digits + string.punctuation))
+
+    # Mezclar la contraseña para que los primeros cuatro caracteres no sean predecibles
+    random.shuffle(contraseña)
+
+    # Convertir la lista a una cadena y devolverla
+    return ''.join(contraseña)
+
+longitud = 10  # puedes cambiar esto a la longitud que prefieras
+print(generar_contraseña(longitud))
